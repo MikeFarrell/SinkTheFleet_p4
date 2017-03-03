cf1372cb86271500dd81dd788b1498dc74dd8c8c
MikeFarrell <m.farrell6102@edmail.edcc.edu>
2017-03-02 16:04:30 -0800

    Update cdirection.h

--- cdirection.h
+++ cdirection.h
@@ -1,22 +1,33 @@
 #ifndef CDIRECTION
 #define CDIRECTION
-
+#include <ostream>
+#include <ios>
+using namespace std;
 #include "oop.h"
 
 namespace DV_STF
 {
-	class CDirection
-	{
-		CDirection();
-
-	private:
-		Direction m_direction;
-	};
+public:
 	
-	enum Direction
-	{
-		HORIZONTAL, // 0
-		VERTICAL	// 1
-	};
+	CDirection() { m_direction = HORIZONTAL;}			// Default constructor
+	~CDirection() {};									// Destructor
+	operator Direction() const { return m_direction; }	// Cast 
+
+	// Print 
+	void print(ostream& os) const { (m_direction == VERTICAL) ? os << 'V' : os << 'H'; }
+	void print(void) const { (m_direction == VERTICAL) ? cout << 'V' : cout << 'H'; }
+	//Related functions?? should these be public?
+	ostream& operator<<(ostream& os) { print(os); return os; }
+	istream& operator>>(istream& is) {}
+private:
+	Direction m_direction;							// HORIZONTAL or VERTICAL
 }
 #endif
+//----------------------------------------------------------------------------
+// enumerated type for direction on grid
+//----------------------------------------------------------------------------
+enum Direction
+{
+	HORIZONTAL, // 0
+	VERTICAL	// 1
+};
