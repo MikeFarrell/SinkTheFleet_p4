0d1406793d6b84699adabdafb979f656ffc7d2f4
jconwhit <jconwhit@gmail.com>
2017-03-04 21:27:33 -0800

    Updated cplayer.h
    added commits from branch with fixes

--- cplayer.h
+++ cplayer.h
@@ -2,6 +2,7 @@
 #define CPLAYER
 #include "cshipinfo.h"
 #include "cship.h"
+#include "cdirection.h"
 
 
 namespace DV_STF
@@ -9,8 +10,35 @@ namespace DV_STF
 	class CPlayer
 	{
 	public:
-		CPlayer() {}
+		CPlayer() {};
+		~CPlayer() {};
+		//copy()									//Deep copy
+		//operator=									//Deep copy assignment
+		//Accessors
 
+		unsigned short getWhichPlayer() const { return m_whichPlayer; }
+		short getPiecesLeft() const { return m_piecesLeft; }
+		char getGridSize() const { return m_gridSize; }
+
+		CShip getCell(short & whichGrid, CCell & cell) const
+		{
+			return m_gameGrid[whichGrid][cell.get_row()][cell.get_col()];
+		}
+
+		void setGridSize(char size) { m_gridSize = size; }
+		void setCell(short & whichGrid, CCell & cell, CShip & ship)
+		{
+			m_gameGrid[whichGrid][cell.get_row()][cell.get_col()] = ship;
+		} // set the proper cell to ship
+
+		  //printGrid(ostream& os);		 
+		  //getGrid(string filename);					// inputs grid from file
+		  //saveGrid();
+		  //setShips();							// allow player to setup ship
+		  //hitShip(CShip ship);						// Decrement pieces left for ship & for fleet
+		  //bool isValidLocation(short whichShip);			// the index # of which ship in array
+		  //operator[] const; 
+		  //operator--();
 	private:
 		unsigned short m_whichPlayer;
 		short m_piecesLeft;
