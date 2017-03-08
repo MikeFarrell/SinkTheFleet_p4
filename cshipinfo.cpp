a692043c4f3472a208c634cad157c29a580e70c5
Jeremiah Robinson <jro.jro1979@gmail.com>
2017-03-07 16:49:18 -0800

    Add files via upload

--- cshipinfo.cpp
+++ cshipinfo.cpp
@@ -1,7 +1,11 @@
 #include "cshipinfo.h"
-using namespace std;
 
 namespace DV_STF
 {
-
+	void CShipInfo::print(ostream & os)
+	{
+		os << "Ship: " << m_name << ". Direction: " << m_orientation
+			<< ". Location: " << m_bowlocation << ". " << m_piecesleft
+			<< " Pieces left." << endl;
+	}
 }
--- cshipinfo.h
+++ cshipinfo.h
@@ -1,3 +1,23 @@
+//#ifndef CSHIPINFO
+//#define CSHIPINFO
+//
+//#include "oop.h"
+//
+//namespace DV_STF
+//{
+//	class CShipInfo
+//	{
+//		CShipInfo(CShip name(), CDirection orientation(), CCell bowlocation(), short piecesleft());
+//		: m_name(name), m_orientation(orientation), m_bowlocation(bowlocation), m_piecesleft(piecesleft)
+//		{}
+//	private:
+//		CShip m_name;
+//		CDirection m_orientation;
+//		CCell m_bowlocation;
+//		short m_piecesleft;
+//	};
+//}
+//#endif
 #ifndef CSHIPINFO
 #define CSHIPINFO
 
@@ -13,14 +33,36 @@ namespace DV_STF
 	class CShipInfo
 	{
 	public:
-		CShipInfo() {}
+		// Constructor & Destructor
+		CShipInfo(CShip name(), CDirection orientation(), CCell bowlocation(), short piecesleft());
+		: m_name(name), m_orientation(orientation), m_bowlocation(bowlocation), m_piecesleft(piecesleft){};
+		CShipInfo() {}; // 
+		~CShipInfo() {};
+		//Prints out the proprties (e.g., "Frigate, located vertically at 
+		//B4--2 pieces left")
+		void print(ostream & os);
+		//with no parameter. Prints to cout.
+		void print(void) { print(cout); }
+		// Accessors
+		CShip getName(void) const { return m_name; }
+		CDirection getOrientation(void) const { return m_orientation; }
+		CCell getBowLocation(void) const { return m_bowlocation; }
+		short getPiecesLeft(void) const { return m_piecesleft; }
+		// Mutators
+		void setName(const CShip name) { m_name = name; }
+		void setOrientation(const CDirection dir) { m_orientation = dir; }
+		void setBowLocation(const CCell location) { m_bowlocation = location; }
+		void setPiecesLeft(const short pieces) { m_piecesleft = pieces; }
 
+		CShipInfo operator--() { m_piecesleft--; }
+		bool isSunk(void) const { return (m_piecesleft == 0) ? true : false; }
+		static const short shipSize[6]; // Holds ship sizes
 	private:
 		CShip m_name;
 		CDirection m_orientation;
 		CCell m_bowlocation;
 		short m_piecesleft;
 	};
+	ostream& operator<<(ostream & os, CShipInfo theShip) { theShip.print(os); }
 }
 #endif
-
