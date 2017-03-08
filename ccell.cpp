2e5327296472827648e614a2d346dcdb02e0e8a8
MikeFarrell <m.farrell6102@edmail.edcc.edu>
2017-03-07 16:49:53 -0800

    Add files via upload

--- ccell.cpp
+++ ccell.cpp
@@ -1,8 +1,116 @@
-#include "ccell.h"
+#include "CCell.h"
+#include "cship.h"
+#include <sstream>
 
-using namespace std;
+//---------------------------------------------------------------------------------
+// Function:	getCoord()
+// Title:	getCoord 
+// Description:
+//		gets the coordinate input and checks for bad input
+// Programmer:	dark vader
+//
+// modified by:	
+// 
+// Date:	3/4/17
+//
+// Version:	1.0
+// 
+// Environment: Hardware: i7 
+//              Software: OS: Windows 7; 
+//              Compiles under Microsoft Visual C++ 2012
+//
+// Input:	grid from specified file
+//
+// Output:	prompts to cout
+//
+// Calls:	n/a
+//
+// Called By:	main()
+//
+// Parameters:	istream& sin, char size
+// 
+// Returns: CCell (row, col)
+//
+// History Log: 
+//				9/12/06 PB comleted v 0.5
+//				3/26/2017 dark vader completed v 0.6
+//				3/27/2017 dark vader completed v 0.7
+//				3/28/2017 dark vader completed v 0.8
+//				3/29/2017 dark vader completed v 0.9
+//				3/04/2017 dark vader completed v 1.0
+//     
+//---------------------------------------------------------------------------------
 
-namespace DV_STF
+
+
+void CCell::inputCoordinates(istream& sin, char size)
 {
 
+	cin.clear();
+	fflush(stdin);
+	short numberOfRows = (toupper(size) == 'L') ? LARGEROWS : SMALLROWS;
+	short numberOfCols = (toupper(size) == 'L') ? LARGECOLS : SMALLCOLS;
+	char highChar = static_cast<char>(numberOfRows + ASCIIA);
+	char row = 0;
+	do
+	{
+		cout << "Row must be a letter from A to " << highChar
+			<< " and column must be  from 1 to " << numberOfCols << ": ";
+		while ((row = toupper(sin.get())) < 'A' || row  > highChar)
+		{
+			sin.ignore(BUFFER_SIZE, '\n');
+			cout << "Row must be a letter from A to " << highChar
+				<< " and column must be  from 1 to " << numberOfCols << ": ";
+		}
+		sin >> m_col;
+		if (!sin)
+			sin.clear();
+		sin.ignore(BUFFER_SIZE, '\n');
+	} while (m_col < 1 || m_col > numberOfCols);
+	m_row = static_cast<unsigned short>(row - 'A');
+	m_col--;
 }
+//---------------------------------------------------------------------------------
+// Function:	ostream& operator<<()
+// Title:	<< operator 
+// Description:
+//		allows the << operator to print things within the CCell class
+// Programmer:	dark vader
+//				
+//				
+// modified by:	dark vader 
+// 
+// Date:	3/4/17
+//
+// Version:	1.0
+// 
+// Environment: Hardware: i7 
+//              Software: OS: Windows 7; 
+//              Compiles under Microsoft Visual C++ 2012
+//
+// Input:	n/a
+//
+// Output:	n/a
+//
+// Calls:	print()
+//
+// Called By:	main()
+//
+// Parameters:	ostream& sout, CCell& cell
+// 
+// Returns: sout
+//
+// History Log: 
+//				9/12/06 PB comleted v 0.5
+//				3/26/2017 dark vader completed v 0.6
+//				3/27/2017 dark vader completed v 0.7
+//				3/28/2017 dark vader completed v 0.8
+//				3/29/2017 dark vader completed v 0.9
+//				3/04/2017 dark vader completed v 1.0
+//     
+//---------------------------------------------------------------------------------
+ostream& operator<<(ostream& sout, CCell& cell)
+{
+	cell.print();
+	return sout;
+};
--- ccell.h
+++ ccell.h
@@ -1,21 +1,81 @@
-#ifndef CCELL
-#define CCELL
+#ifndef FLEET_H
+#define FLEET_H
+
+#include <iostream>
+#include <fstream>
+#include <cctype>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include "textGraphics.h"
+#include "safeio.h"
 
 using namespace std;
-namespace DV_STF
+//---------------------------------------------------------------------------
+//	Title:          CCell.h
+//	Description:    This file contains the class definition for CCell
+//	Programmer:     dark vader
+//	Date:           3/4/2017
+//	Version:	1.00
+//	Environment:	 Hardware: i7 
+//              Software: OS: Windows 7; 
+//              Compiles under Microsoft Visual C++ 2012
+//	class CShip:
+//	Properties:
+//			m_row - the row of the coordinate
+//			m_col - the column of the coordinate
+//	Methods:
+//		Inline:
+//			Constructor CCell(unsigned short row = 0, short col = 0)
+//			Accessors: unsigned short getRow()
+//					   unsigned short getCol()
+//			Void Print()
+//		Non-Inline:
+//				void CCell::getCoord(istream& sin, char size);
+//				ostream& operator<<(ostream& sout, CCell& cell);
+//				
+//  History Log:
+//				9/12/06 PB comleted v 0.5
+//				3/26/2017 dark vader completed v 0.6
+//				3/27/2017 dark vader completed v 0.7
+//				3/28/2017 dark vader completed v 0.8
+//				3/29/2017 dark vader completed v 0.9
+//				3/04/2017 dark vader completed v 1.0
+//     
+//-----------------------------------------------------------------------------
+class CCell
 {
-	class CCell
-	{
-	public:
-		CCell() {};
-		~CCell() {};
-		unsigned short get_row() { return m_row; }
-		unsigned short get_col() { return m_col; }
+public:
+	static const short NUMPLAYERS = 2;	// number of players in game
+	static const short SMALLROWS = 8;	// number of rows/columns in small sea array 
+	static const short LARGEROWS = 10;	// number of rows/columns in large sea array
+	static const short SMALLCOLS = 12;	// number of rows/columns in small sea array 
+	static const short LARGECOLS = 24;	// number of rows/columns in large sea array
+	static const short BOXWIDTH = 79;	// maximum screen line length
+	static const short ASCIIA = 65; // value of A in ASCII
+	static const short ONE = 1;
 
-	private:
-		unsigned short m_row;
-		unsigned short m_col;
-	};
-}
+	CCell(unsigned short row = 0, unsigned short col = 0)
+	{
+		m_row = row; m_col = col;
+	}
+	void printCoord(ostream& sout)
+	{
+		cout << static_cast<char>(m_row + ASCIIA + ONE) << m_col + ONE;
+	}
+	void inputCoordinates(istream& sin, char size);
+	unsigned short get_row() { return m_row; }
+	unsigned short get_col() { return m_col; }
+	void print() { printCoord(cout); }
+	void increaseCol() { m_col++; }
+	void increaseRow() { m_row++; }
+
+private:
+	unsigned short m_row;	// row
+	unsigned short m_col;	// column
+};
+ostream& operator<<(ostream& sout, CCell& cell);
+
 #endif
+
 
