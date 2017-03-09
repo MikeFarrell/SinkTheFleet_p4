8286e7be2e7bbf31009b56aecdf68086916d9338
MikeFarrell <m.farrell6102@edmail.edcc.edu>
2017-03-09 11:17:53 -0800

    comments added

--- cdirection.cpp
+++ cdirection.cpp
@@ -4,11 +4,37 @@ using namespace std;
 
 namespace DV_STF
 {
+	//----------------------------------------------------------------------------
+	//	Class:         CPlayer
+	//	method:        operator<<(ostream& os, const CDirection & dir)
+	//	description:   formated output 
+	//	Input:         None 
+	//	Output:        N/A
+	//	Calls:         N/A
+	//	Called By:     N/A
+	//	Parameters:	   ostream& os, const CDirection & dir
+	//	Returns:       os
+	//	History Log:
+	//	               2017-03-08 DV  completed version 1.0
+	//----------------------------------------------------------------------------
 	ostream& operator<<(ostream& os, const CDirection & dir)
 	{
-		dir.print(os); return os; 
+		dir.print(os); return os;
 	}
 
+	//----------------------------------------------------------------------------
+	//	Class:         CPlayer
+	//	method:        operator >> (istream& is, CDirection & dir)
+	//	description:   format input 
+	//	Input:         None 
+	//	Output:        N/A
+	//	Calls:         N/A
+	//	Called By:     N/A
+	//	Parameters:	   istream& is, CDirection & dir
+	//	Returns:       is
+	//	History Log:
+	//	               2017-03-08 DV  completed version 1.0
+	//----------------------------------------------------------------------------
 	istream& operator >> (istream& is, CDirection & dir)
 	{
 		return is;
